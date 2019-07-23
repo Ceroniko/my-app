24737ce19d8c93163f72705a7b0d11a5632e2cef
Ceroniko <ikomaristiy@gmail.com>
2019-07-23 15:38:42 +0300

    my app1

--- src/routing/Root.js
+++ src/routing/Root.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from '../home/Home.js';
 import Testing from '../testing/Testing';
 import App from '../app/App.js';
