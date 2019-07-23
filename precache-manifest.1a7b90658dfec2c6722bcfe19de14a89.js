1f154a108ba001131fb4e6e0410132e5900bfc73
Ceroniko <ikomaristiy@gmail.com>
2019-07-23 15:35:31 +0300

    Updates

---
+++ precache-manifest.1a7b90658dfec2c6722bcfe19de14a89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68ab297a6e7416522c928575ed31e695",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "276ee239ed618fd356f0",
+    "url": "/my-app/static/css/main.0e9ff973.chunk.css"
+  },
+  {
+    "revision": "81f9873d60a1083e7af4",
+    "url": "/my-app/static/js/2.9462c2bd.chunk.js"
+  },
+  {
+    "revision": "276ee239ed618fd356f0",
+    "url": "/my-app/static/js/main.f5123579.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  }
+]);
